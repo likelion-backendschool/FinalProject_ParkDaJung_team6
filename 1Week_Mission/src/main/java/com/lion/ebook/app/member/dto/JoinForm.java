d37f4aebeb781978d31dcd515cd81fbade73928d
박다정 <ekwjd990913@gmail.com>
2022-10-17 16:48:39 +0900

    [회원] 회원가입, 로그인 화면 생성

---
+++ 1Week_Mission/src/main/java/com/lion/ebook/app/member/dto/JoinForm.java
@@ -0,0 +1,19 @@
+package com.lion.ebook.app.member.dto;
+
+import lombok.Data;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class JoinForm {
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String password;
+
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String nickname;
+}
--- 1Week_Mission/src/main/java/com/lion/ebook/app/member/repositoy/MemberRepository.java
+++ 1Week_Mission/src/main/java/com/lion/ebook/app/member/repositoy/MemberRepository.java
@@ -3,7 +3,12 @@ package com.lion.ebook.app.member.repositoy;
 import com.lion.ebook.app.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    boolean existsByUsername(String username);
+
+    Member findByUsername(String username);
 }
