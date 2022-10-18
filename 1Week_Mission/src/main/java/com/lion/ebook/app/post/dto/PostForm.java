81c384633ed86d02802b1890a3af1d5b828f765e
박다정 <ekwjd990913@gmail.com>
2022-10-19 01:25:14 +0900

    [게시물] 게시물 작성 완료

---
+++ 1Week_Mission/src/main/java/com/lion/ebook/app/post/dto/PostForm.java
@@ -0,0 +1,19 @@
+package com.lion.ebook.app.post.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class PostForm {
+
+    @NotEmpty
+    private String subject;
+
+    @NotEmpty
+    private String content;
+
+    private String hashTagContents;
+}
