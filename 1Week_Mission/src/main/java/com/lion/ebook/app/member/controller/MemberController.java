f5bc6d28f520dc258d3aaaa72d657b1a30c74e2b
박다정 <ekwjd990913@gmail.com>
2022-10-19 15:41:25 +0900

    프로필 추가

--- 1Week_Mission/src/main/java/com/lion/ebook/app/member/controller/MemberController.java
+++ 1Week_Mission/src/main/java/com/lion/ebook/app/member/controller/MemberController.java
@@ -7,6 +7,7 @@ import com.lion.ebook.common.dto.ResultData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -60,6 +61,12 @@ public class MemberController {
         }
 
         return new ResultData<>("200", "성공", member.getUsername());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/profile")
+    public String getMypage() {
+        return "member/profile";
     }
 
 }
